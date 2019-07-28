e1167dfe9149f842d20a9427a1543cef2f7a0647
Peter Yoon <peyo.hd@gmail.com>
2019-07-28 19:13:58 +0900

    switch to HIDL graphics HAL

--- rpi3.mk
+++ rpi3.mk
@@ -24,7 +24,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libGLES_mesa \
     gralloc.rpi3 \
-    hwcomposer.rpi3 \
     memtrack.rpi3 \
     audio.primary.rpi3 \
     audio.usb.default \
@@ -35,10 +34,9 @@ PRODUCT_PACKAGES += \
 
 # hardware/interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.allocator@2.0-service.arpi \
+    android.hardware.graphics.mapper@2.0-impl.arpi \
+    android.hardware.graphics.composer@2.1-impl.arpi \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.keymaster@3.0-impl \
